459dfcaff4f4467c8ecdafe159e1494d23724743
Maryam Moataz <73883092+MaryamMoataz@users.noreply.github.com>
2023-01-03 14:11:53 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,18 @@ from PyQt5 import uic
 import sys  # We need sys so that we can pass argv to QApplication
 import qdarktheme
 import magic
+import Bio
 from Bio import SeqIO
 from localalign import localAllignment
 from globalalign import globalAllignment
 from muscle import muscle
-
+from PyQt5.uic import loadUi
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+import numpy as np
+import random
+from plots.dotplot import dotplot
+from plots.logo import sequence_logo
+from plots.phylogenetic import phylogenetic_tree
 
 #error messg maybe add in try except?
 # msg = QMessageBox() 
@@ -29,6 +36,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
 
         self.browse_button.clicked.connect(self.browse)
+        self.view_alignment_button.clicked.connect(self.align)
         self.sequences = []
 
     # We will use bio python for the phylogenetic tree
@@ -41,16 +49,18 @@ class MainWindow(QtWidgets.QMainWindow):
     def browse(self):
         #match_score , mismatch_score, gap_score
         self.fname = QFileDialog.getOpenFileName(self, 'Open file', ''," files (*.fasta *.txt )")
+        with open(self.fname[0], 'r') as f:
+            data = f.read()
         chosen_file = magic.from_file(self.fname[0],mime=True)
 
-        self.sequences = list(SeqIO.parse(self.fname[0], "fasta"))
+        self.sequences = list(Bio.SeqIO.parse(self.fname[0], "fasta"))
+
+        self.enter_seq_text.setText(f'{str(data)}')
     
-        match_score = int(self.match_score_text.toPlainText())
-        mismatch_score = int(self.mismatch_score_text.toPlainText())
-        gap_score = int(self.gap_score_text.toPlainText())
-        # Metrics to test the multiple sequenc
-        # I asked chatGPT and he said Percentage of Identical Pairs and Percentage of Conserved Pairs 
-        # How to calculate Percentage of Identical Pairs of multiple sequence alignment ?
+
+    def align(self):
+
+
         '''
         To calculate the percentage of identical pairs in a multiple sequence alignment, you can follow these steps:
 
@@ -59,46 +69,49 @@ class MainWindow(QtWidgets.QMainWindow):
             3- Divide the count of identical pairs by the total number of pairs in the alignment and multiply by 100 to get the percentage of identical pairs.
         ''' 
 
-        
 
         if len(self.sequences) > 2: 
             multiple_alignment = muscle(self.fname[0])
-            self.aligned_seq_text = multiple_alignment
-            # Let's assume we have an MSA stored in a list of strings called 'sequences'
-            num_identical_pairs = 0
-            num_total_pairs = len(multiple_alignment) * (len(multiple_alignment) - 1) / 2
-            
-            #Percent identity is calculated by dividing the number 
-            #of nucleotide matches in the alignment by the total number of nucleotides (matched or mismatched)
-            for i in range(len(multiple_alignment)): # Loop in every sequence
-                for j in range(i + 1, len(multiple_alignment)): # Loop in every other sequence
-                    for k in range(len(multiple_alignment[i])): # Loop in every position
-                        if multiple_alignment[i][k] == multiple_alignment[j][k]: # If the residues at a given position are the same, increment a count of identical pairs.
-                            num_identical_pairs += 1
+            with open('./data/alig.fasta', 'r') as f:
+                self.aligned_seq = f.read()
+            self.aligned_seq_text.setText(f'{str(self.aligned_seq)}')  
 
-            percentage_identical_pairs = (num_identical_pairs / num_total_pairs) * 100
+            # sequence_logo(self) 
+            phylogenetic_tree(self)
+
+            
+            # Let's assume we have an MSA stored in a list of strings called 'sequences'
+            # num_identical_pairs = 0
+            # num_total_pairs = len(multiple_alignment) * (len(multiple_alignment) - 1) / 2
+            
+            # for i in range(len(multiple_alignment)): # Loop in every sequence
+            #     for j in range(i + 1, len(multiple_alignment)): # Loop in every other sequence
+            #         for k in range(len(multiple_alignment[i])): # Loop in every position
+            #             if multiple_alignment[i][k] == multiple_alignment[j][k]: # If the residues at a given position are the same, increment a count of identical pairs.
+            #                 num_identical_pairs += 1
+
+            # percentage_identical_pairs = (num_identical_pairs / num_total_pairs) * 100
+            
 
         elif len(self.sequences) == 2:
+            match_score = int(self.match_score_box.value())
+            mismatch_score = int(self.mismatch_score_box.value())
+            gap_score = int(self.gap_score_box.value())
+
             localaligned = localAllignment(match_score,mismatch_score,gap_score,self.sequences[0].seq,self.sequences[1].seq)
             globalaligned = globalAllignment(match_score,mismatch_score,gap_score,self.sequences[0].seq,self.sequences[1].seq)
             if self.local_radio_button.isChecked():
-                self.aligned_seq_text = localaligned
+                self.aligned_seq = localaligned
             else:
-                self.aligned_seq_text = globalaligned
+                self.aligned_seq = globalaligned
 
-                
-        #cursor parking
-        #-----------#
-        #           #
-        #           #                    
-        #-----------#
-        # Stick man #      
-        #-----------#
-        #     0     #
-        #    /|\    # 
-        #    / \    #                    
-        #-----------#
-    # Guess th 
+            data = self.aligned_seq[0] + '\n' + self.aligned_seq[1]
+            self.aligned_seq_text.setText(f'{str(data)}') 
+
+            dotplot(self,self.aligned_seq[0],self.aligned_seq[1])   
+
+        
+
 
 app = QtWidgets.QApplication(sys.argv)
 w = MainWindow()
