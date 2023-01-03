3469f7af0e216ed6ddaa2d7584ecb2add51423c5
Maryam Moataz <73883092+MaryamMoataz@users.noreply.github.com>
2023-01-03 17:00:37 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ import random
 from plots.dotplot import dotplot
 from plots.logo import sequence_logo
 from plots.phylogenetic import phylogenetic_tree
-from stats import perc_identity
+
+from stats import percent_identity_two
 #error messg maybe add in try except?
 # msg = QMessageBox() 
 # msg.setIcon(QMessageBox.Critical)
@@ -38,6 +39,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.browse_button.clicked.connect(self.browse)
         self.view_alignment_button.clicked.connect(self.align)
         self.sequences = []
+        self.imported=0
 
     # We will use bio python for the phylogenetic tree
     # We already did dot plot
@@ -47,27 +49,38 @@ class MainWindow(QtWidgets.QMainWindow):
     # let try https://en.wikipedia.org/wiki/Sequence_logo 
     # Or Dot plot
     def browse(self):
-        #match_score , mismatch_score, gap_score
-        self.fname = QFileDialog.getOpenFileName(self, 'Open file', ''," files (*.fasta *.txt )")
-        with open(self.fname[0], 'r') as f:
-            data = f.read()
-        chosen_file = magic.from_file(self.fname[0],mime=True)
+        try:
+            self.fname = QFileDialog.getOpenFileName(self, 'Open file', ''," files (*.fasta)")
+            with open(self.fname[0], 'r') as f:
+                data = f.read()
+            chosen_file = magic.from_file(self.fname[0],mime=True)
 
-        self.sequences = list(Bio.SeqIO.parse(self.fname[0], "fasta"))
+            self.sequences = list(Bio.SeqIO.parse(self.fname[0], "fasta"))
 
-        self.enter_seq_text.setText(f'{str(data)}')
+            self.enter_seq_text.setText(f'{str(data)}')
+
+            self.imported=1
+        except:
+
+            msg = QMessageBox() 
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Error")
+            msg.setInformativeText('Please choose correct file or enter sequence')
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+
+
+
+        # else:
+
+        #     self.sequences = list(Bio.SeqIO.parse(self.enter_seq_text.value))
     
 
     def align(self):
 
-
-        '''
-        To calculate the percentage of identical pairs in a multiple sequence alignment, you can follow these steps:
-
-            1- Align the sequences using a multiple sequence alignment algorithm.
-            2- For each pair of aligned sequences, compare each position in the alignment. If the residues at a given position are the same, increment a count of identical pairs.
-            3- Divide the count of identical pairs by the total number of pairs in the alignment and multiply by 100 to get the percentage of identical pairs.
-        ''' 
+       if self.imported==0 and self.enter_seq_text.value !="": 
+        self.sequences = list(Bio.SeqIO.parse(self.enter_seq_text.value, "fasta")) #probably wrong needs path lol
 
 
         if len(self.sequences) > 2: 
@@ -78,19 +91,8 @@ class MainWindow(QtWidgets.QMainWindow):
 
             # sequence_logo(self) 
             phylogenetic_tree(self)
-            perc_identity(self.aligned_seq)
-            
-            # Let's assume we have an MSA stored in a list of strings called 'sequences'
-            # num_identical_pairs = 0
-            # num_total_pairs = len(multiple_alignment) * (len(multiple_alignment) - 1) / 2
-            
-            # for i in range(len(multiple_alignment)): # Loop in every sequence
-            #     for j in range(i + 1, len(multiple_alignment)): # Loop in every other sequence
-            #         for k in range(len(multiple_alignment[i])): # Loop in every position
-            #             if multiple_alignment[i][k] == multiple_alignment[j][k]: # If the residues at a given position are the same, increment a count of identical pairs.
-            #                 num_identical_pairs += 1
+            percent_identity_two()
 
-            # percentage_identical_pairs = (num_identical_pairs / num_total_pairs) * 100
             
 
         elif len(self.sequences) == 2:
--- stats.py
+++ stats.py
@@ -1,16 +1,78 @@
 
+import os
+from Bio import SeqIO
+from Bio.Seq import Seq
 from Bio import AlignIO
+from Bio.SeqRecord import SeqRecord
+from math import log
+
+
+from Bio.Align import MultipleSeqAlignment
+import time
+# def perc_identity(align):
+
+#     # a = SeqRecord(Seq("CCAAGCTGAATCAGCTGGCGGAGTCACTGAAACTGGAGCACCAGTTCCTAAGAGTTCCTTTCGAGCACTACAAGAAGACGATTCGCGCGAACCACCGCAT"), id="Alpha")
+#     # b = SeqRecord(Seq("CGAAGCTGACTCAGTGGGCGGAGTCACTGAAACTGGAGCACCAGTTCCTCAGAGTCCCCTTCGAGCACTACAAGAAGACAATTCGTGCGAACCACCGCAT"), id="Beta")
+#     # c = SeqRecord(Seq("CGAAGCTGACTCAGTTGGCAGAATCACTGAAACTGGAGCACCAGTTCCTCAGAGTCCCCTTCGAGCACTACAAGAAGACGATTCGTGCGAACCACCGCAT"), id="Gamma")
+#     # d = SeqRecord(Seq("CGAAGCTGACTCAGTTGGCAGAGTCACTGAAACTGGAGCACCAGTTCCTCAGAGTCCCCTTCGAGCACTACAAGAAGACGATTCGTGCGAACCACCGCAT"), id="Delta")
+#     # e = SeqRecord(Seq("CGAAGCTGACTCAGTTGGCGGAGTCACTGAAACTGGAGCACCAGTTCCTCAGAGTCCCCTTCGAGCACTACAAGAAGACGATTCGTGCGAACCACCGCAT"), id="Epsilon")
+
+#     # align = MultipleSeqAlignment([a, b, c], annotations={"tool": "demo"})
+
+#     start_time = time.time()
+#     if len(align) != 1:
+#         for n in range(0,len(align[0])):
+#             n=0
+#             i=0
+#             while n<len(align[0]):    
+#                 column = align[:,n]
+#                 if (column == len(column) * column[0]) == True:
+#                     i=i+1
+#                 n=n+1
+
+#         match = float(i)
+#         length = float(n)
+#         global_identity = 100*(float(match/length))
+#         print(global_identity)
+
+#     print("--- %s seconds ---" % (time.time() - start_time))
 
 
 
+# def MI(sequences,i,j):
+#     Pi = Counter(sequence[i] for sequence in sequences)
+#     Pj = Counter(sequence[j] for sequence in sequences)
+#     Pij = Counter((sequence[i],sequence[j]) for sequence in sequences)   
 
-def perc_identity(aln):
-    i = 0
-    for a in range(0,len(aln[0])):
-        s = aln[:,a]
-        if s == len(s) * s[0]:
-            i += 1
-    return 100*i/float(len(aln[0]))
+#     print(sum(Pij[(x,y)]*log(Pij[(x,y)]/(Pi[x]*Pj[y])) for x,y in Pij))
+
+def percent_identity_two():
+    records = list(SeqIO.parse("./aligned.fasta"))
+    
+    total_pairs=0
+    sop=0
+    count=0
+    countall=0 
+    for i in range(len(records)): #bamsek 1 sequence
+        for j in range(i+1,len(records)): #bamsek el ta7to
+            seq1=records[i].seq #records le awl sequence
+            seq2=records[j].seq #records for other sequences
+                    #For percent identity analysis
+            count,countall=sum_of_pairs(seq1,seq2)
+            total_pairs+=countall
+            sop=sop+count
+    percent_Identity=sop/total_pairs*100 
+    print(percent_Identity)
+
+def sum_of_pairs(a,b):
+        count = 0
+        all_pairs_count=0
+        for x, y in zip(a, b):
+            all_pairs_count+=1
+            if x == y:
+                count += 1
+        print(all_pairs_count)   
+        return count,all_pairs_count
 
 
     
